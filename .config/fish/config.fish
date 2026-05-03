4243333088522d5b7abf9cb034a55e7b907c3d4d
Nikita Sokolov <nekotsokolov2001@gmail.com>
2026-05-03 19:44:03 +0300

    Stable config niri+noctalia

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+export EDITOR=vim
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
